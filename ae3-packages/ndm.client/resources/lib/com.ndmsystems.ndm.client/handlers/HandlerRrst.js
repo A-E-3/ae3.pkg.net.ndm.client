0728500bbdf42e0c81a5c1759dfa0d18033c3853
Alexander I. Kharitchev <myx@myx.co.nz>
2021-05-20 16:03:31 +0300

    * minor: HandlerRrst, work in progress

--- ae3-packages/ndm.client/resources/lib/com.ndmsystems.ndm.client/handlers/HandlerRrst.js
+++ ae3-packages/ndm.client/resources/lib/com.ndmsystems.ndm.client/handlers/HandlerRrst.js
@@ -7,25 +7,17 @@ const RemoteServiceStateSAPI = require("java.class/ru.myx.ae3.state.RemoteServic
  * 
 **/
 const HandlerRrst = module.exports = function(message, address, serial){
-	this.sendSingle(new UdpCloudService.MsgCerr(serial, 0x01 /* No Such Component */), address);
-	return;
-	/**
-	 * FIXME: implement, use ru.myx.ae3.state
-	 */
-	const component = this.client.components[message.component];
-	if(undefined === component || !component.AbstractComponent){
-		console.log(">>>>>> ndm.client:UdpCloudService::handlerRrst: invalid component: %s", message.component);
-		this.sendSingle(new UdpCloudService.MsgCerr(serial, 0x01 /* No Such Component */), address);
-		return;
-	}
-	const args = message.argument.toStringUtf8().split('\n');
-	const result = component.prepareCall(args);
-	if(!result){
-		console.log(">>>>>> ndm.client:UdpCloudService::handlerRrst(%s, %s) => CERR: %s", this, message, component.componentName);
+	const rrst = message.rrst;
+	if(!rrst){
+		console.log(">>>>>> ndm.client:UdpCloudService::handlerRrst(%s, %s) => CERR: no rrst body", this, message);
 		this.sendSingle(new UdpCloudService.MsgCerr(serial, 0x03 /* Invalid Arguments */), address);
 		return;
 	}
-	console.log(">>>>>> ndm.client:UdpCloudService::handlerRrst(%s, %s) => RSST, %s", this, message, component.componentName);
+	
+	console.log(">>>>>> ndm.client:UdpCloudService::handlerRrst(%s, %s) => RSST, %s", this, message, rrst);
+	this.sendSingle(new UdpCloudService.MsgCerr(serial, 0x01 /* No Such Component */), address);
+	return;
+
 	this.sendSingle(new UdpCloudService.MsgRsst(/** FIXME: TODO: */ RSST.makeReply(message.rrst), serial), address);
 	setTimeout(result, 0);
 };
