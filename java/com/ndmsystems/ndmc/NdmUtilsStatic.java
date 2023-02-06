50ad4ab39ba9169e7100a8a5b8f10a0c96e648f6
Alexander Kharichev <myx@myx.ru>
2023-02-06 15:14:24 +0200

    * readTextFirstColumnToMapKeys - faster for large files

--- java/com/ndmsystems/ndmc/NdmUtilsStatic.java
+++ java/com/ndmsystems/ndmc/NdmUtilsStatic.java
@@ -2,51 +2,76 @@ package com.ndmsystems.ndmc;
 
 import java.io.IOException;
 import java.util.ConcurrentModificationException;
+import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
-import ru.myx.ae3.base.BaseMap;
 import ru.myx.ae3.base.BaseObject;
 import ru.myx.ae3.binary.TransferCopier;
+import ru.myx.ae3.exec.ExecProcess;
+import ru.myx.ae3.reflect.ReflectionContextArgument;
 import ru.myx.ae3.reflect.ReflectionExplicit;
 import ru.myx.ae3.reflect.ReflectionManual;
 
 /** @author myx */
 @ReflectionManual
 public class NdmUtilsStatic {
-
-	/** @param binary
+	
+	/** @param process
+	 * @param binary
+	 *            - source binary with text lines
 	 * @param map
+	 *            - target map, <String, Object>
 	 * @param keyExactLength
+	 *            - the exact length of the key column
 	 * @param value
+	 *            - value to use for items put in map
+	 * @param errorTemplate
+	 *            - null or error template for invalid lines (must contain one '%s')
 	 * @return
 	 * @throws IOException
 	 * @throws ConcurrentModificationException */
 	@ReflectionExplicit
-	public static final int readTextFirstColumnToMapKeys(final TransferCopier binary, final BaseMap map, final int keyExactLength, final BaseObject value)
-			throws ConcurrentModificationException, IOException {
+	@ReflectionContextArgument
+	public static final int readTextFirstColumnToMapKeys(//
+			final ExecProcess process,
+			final TransferCopier binary,
+			final Map<String, Object> map,
+			final int keyExactLength,
+			final BaseObject value,
+			final String errorTemplate//
+	) throws ConcurrentModificationException, IOException {
 
-		final Scanner scanner = new Scanner(binary.nextReaderUtf8());
-		scanner.useDelimiter("\n");
-		
 		int count = 0;
-		
-		for (; scanner.hasNext();) {
-			final String line = scanner.next();
-			final int length = line.length();
-			if (length == 0) {
-				continue;
-			}
-			if (length == keyExactLength) {
-				map.put(line, value);
-				++count;
-				continue;
-			}
-			if (length > keyExactLength && -1 != " \r\t,".indexOf(line.charAt(keyExactLength))) {
-				map.put(line.substring(0, keyExactLength), value);
-				++count;
-				continue;
+
+		try (final Scanner scanner = new Scanner(binary.nextReaderUtf8());) {
+			scanner.useDelimiter("\n");
+			
+			for (;;) {
+				final String line;
+				try {
+					line = scanner.next();
+				} catch (final NoSuchElementException e) {
+					return count;
+				}
+				final int length = line.length();
+				if (length == 0) {
+					continue;
+				}
+				if (length == keyExactLength) {
+					map.put(line, value);
+					++count;
+					continue;
+				}
+				if (length > keyExactLength && -1 != " \r\t,".indexOf(line.charAt(keyExactLength))) {
+					map.put(line.substring(0, keyExactLength), value);
+					++count;
+					continue;
+				}
+				if (line.trim().length() != 0 && null != errorTemplate) {
+					process.getConsole().error(errorTemplate, line);
+				}
 			}
 		}
-		return count;
 	}
 }
